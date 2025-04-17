37e5b00303ce02669d7b9b102584aa04a6fa3a85
vic87207 <vic87207@gmail.com>
2025-04-17 16:34:18 -0400

    fixed the url from "/deals" to ""

--- deal_tracker/urls.py
+++ deal_tracker/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
-    path("deals/", include("deals.urls")),
+    path("", include("deals.urls")),
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page='login'), name="logout"),
